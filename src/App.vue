e85f28a59faf5d812a3797c3eeaed88975dbfbb9
Purachet Zheng <poorachad2545@gmail.com>
2022-02-21 19:13:07 +0700

    simple layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref,reactive } from 'vue'
+import { ref, reactive } from 'vue'
 const users = reactive([
   {
     id: 1,
@@ -19,127 +19,73 @@ const newUserName = ref('')
 const addNew = () => {
   users.push({
     id: newUserID.value,
-    name: newUserName.value
+    name: newUserName.value,
   })
 }
 const delUser = (id) => {
   console.log(id)
-  users.splice(users.findIndex(ele => ele.id == id),1)
+  users.splice(
+    users.findIndex((ele) => ele.id == id),
+    1
+  )
 }
 </script>
 
 <template>
-
-  <div id="container">
-    <div id="header">
-      <input type="text" class="mr-2" v-model="newUserID" placeholder="id">
-      <input type="text" class="mr-2" v-model="newUserName" placeholder="Name">
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" @click="addNew">+</button>
+  <div id="container" class="bg-gray-600 my-0 mx-auto">
+    <!-- header -->
+    <div class="bg-green-600 h-16 flex justify-center items-center">
+      <h2 class="text-4xl text-green-100">head</h2>
     </div>
-    <div id="wrapper">
-      <div id="content">
-        <table class="table-auto">
+
+    <!-- content & aside -->
+    <div class="lg:flex h-full item-center item-stretch">
+
+      <div class="bg-orange-500 p-10 sm:w-full lg:w-9/12">
+        <h2>Table</h2>
+        <!-- <table class="table-auto">
           <thead>
             <tr>
-              <th>id</th>
-              <th>Name</th>
+              <th>Song</th>
+              <th>Artist</th>
+              <th>Year</th>
             </tr>
           </thead>
           <tbody>
-            <tr v-for="(user,i) in users">
-              <td>{{user.id}} </td>
-              <td>{{user.name}} </td>
-              <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full" @click="delUser(user.id)">-</button>
+            <tr>
+              <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+              <td>Malcolm Lockyer</td>
+              <td>1961</td>
+            </tr>
+            <tr>
+              <td>Witchy Woman</td>
+              <td>The Eagles</td>
+              <td>1972</td>
+            </tr>
+            <tr>
+              <td>Shining Star</td>
+              <td>Earth, Wind, and Fire</td>
+              <td>1975</td>
             </tr>
           </tbody>
-        </table>
+        </table> -->
+      </div>
+
+      <div class="bg-teal-500 p-10 sm:w-full lg:w-3/12">
+        <h2>sidebar</h2>
       </div>
     </div>
-    <div id="navigation">
-      <p><strong>2. Navigation Menu</strong></p>
-      <h3>Styling Flex Container</h3>
-      <ul>
-        <li>flex-direction</li>
-        <li>flex-wrap</li>
-        <li>justify-content</li>
-        <li>align-content</li>
-        <li>align-items</li>
-      </ul>
-      <h3>Designing Flex Items</h3>
-      <ul>
-        <li>order</li>
-        <li>align-self</li>
-        <li>flex-grow</li>
-        <li>flex-shrink</li>
-        <li>flex-basis</li>
-      </ul>
+
+    <!-- footer -->
+    <div class="p-10">
+      <h2>footer</h2>
     </div>
-    <div id="extra">
-      <p><strong>3. Additional Stuff</strong></p>
-      <h3>In this guide you will learn:</h3>
-      <ul>
-        <li>How to use CSS Flexbox to build modern layouts</li>
-        <li>How to align items both vertically and horizontally</li>
-        <li>How to order items within container</li>
-        <li>And more</li>
-      </ul>
-    </div>
-    <div id="footer"><p>Footer</p></div>
   </div>
 </template>
 
 <style>
-html,
-body {
+* {
   margin: 0;
   padding: 0;
-}
-div#header {
-  position: relative;
-  color: #292929;
-  padding-left: 10px;
-  background: #e0e0e0;
-}
-
-div#navigation {
-  background: #7cb71c;
-}
-
-div#navigation li {
-  list-style: none;
-}
-
-div#extra {
-  background: #147fa9;
-}
-
-div#footer {
-  background: #42444e;
-  color: #fff;
-}
-
-div#footer p {
-  padding: 20px 10px;
-} 
-
-div#content {
-  float: left;
-  width: 500px;
-}
-
-div#navigation {
-  float: right;
-  width: 200px;
-}
-
-div#extra {
-  float: right;
-  clear: right;
-  width: 200px;
-}
-
-div#footer {
-  clear: both;
-  width: 100%;
 }
 </style>
